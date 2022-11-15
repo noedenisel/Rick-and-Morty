dea7cce04702947315cca8e9ebd6812e5bfaa6ec
Noe Denise <noelialombardo@hotmail.com>
2022-11-15 20:22:44 -0300

    Dandole styles

---
+++ src/components/Cards.jsx
@@ -0,0 +1,13 @@
+import Card from './Card';
+
+export default function Cards(props) {
+   const { characters } = props;
+   return (
+   <div>
+      {
+       characters.map(c => (
+       <Card name={c.name} species={c.species} gender={c.gender} image={c.image} onClose= {c.onClose}/>
+       )) } 
+  </div>
+)}
+
